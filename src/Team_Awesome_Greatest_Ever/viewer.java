700a8d7626b011acda073fa8be9a5d326b09c4ed
Matthew Buckley <mbuckley2017@2017_matthew_buckley.mka.org>
2016-06-03 08:37:14 -0400

    viewer class for pizzaProjectBlock1

---
+++ src/Team_Awesome_Greatest_Ever/viewer.java
@@ -0,0 +1,12 @@
+package Team_Awesome_Greatest_Ever;
+
+import javax.swing.JFrame;
+
+public class viewer {
+	public static void main(String[] args) {
+		JFrame frame = new PizzaFrame(); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
